c2fe5df82aae678f3c0af763b40fe6e95aa9c610
Arpitajoshi88[A <arpitajoshi.xworkz@gmail.com>
2022-03-03 12:32:53 +0530

    Puzzle Program examples

---
+++ Puzzle/Tester.java
@@ -0,0 +1,38 @@
+public class Tester {
+ 
+	public static void main(String []args) 
+	{
+	     /* String res= Puzzle.name;
+	      System.out.println(res);
+	      
+	      int lengt=res.length();  
+	      System.out.println(lengt);
+	     
+	      
+	  
+	     String replace= res.replace("Dice","chess");
+	      System.out.println(replace);*/
+
+
+	      
+	     String okok = "sit,out,stand";
+		String [] a = okok.split(",");
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		
+		System.out.println(okok.isEmpty());
+		String b = "";
+		System.out.println(b.isEmpty());
+
+          String cad = "Autocad";
+          System.out.println(cad.arrayChar[1]);
+		
+
+
+
+}         
+
+
+			
+}
